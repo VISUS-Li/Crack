d929af2b476a341f613e18526e31a06103001c7d
VISUS-Li <1183989659@qq.com>
2020-07-08 20:21:41 +0800

    删除aes加密和excel操作

---
+++ yiplus/TAesClass.cpp
@@ -0,0 +1,6 @@
+#include "aeshelper.h"
+
+AesHelper::AesHelper()
+{
+
+}
---
+++ yiplus/TAesClass.h
@@ -0,0 +1,11 @@
+#ifndef AESHELPER_H
+#define AESHELPER_H
+
+
+class AesHelper
+{
+public:
+    AesHelper();
+};
+
+#endif // AESHELPER_H
