16a795eb947a7b93e9ded0f05154c62d64a24ebc
VISUS-Li <1183989659@qq.com>
2021-08-24 20:36:05 +0800

    修改1

--- xiaomiyoupin/crack_program/login.py
+++ xiaomiyoupin/crack_program/login.py
@@ -12,17 +12,18 @@ from urllib.parse import urlencode
 # }
 proxies = {}
 
+
 def service_login(dev_id, phone):
     url = "https://account.xiaomi.com/pass/serviceLogin?_json=true&sid=miotstore"
     headers = {
-        "User-Agent":"Dalvik/2.1.0 (Linux; U; Android 8.1.0; MI 8 MIUI/8.6.18) APP/xiaomi.youpin APPV/300 MK/TUkgOA== "
-                     "PassportSDK/3.3.5 passport-ui/3.3.5 Dalvik/2.1.0 (Linux; U; Android 8.1.0; MI 8 "
-                     "MIUI/8.6.18)Mobile MIOTWeex/2.0.2 ("
-                     "YouPin;4.15.0;0D2F912ED0CA5ECA432C98814A0F1842;0.20.1;A"
-                     ";49CB1FE12BE10DDE7B6D5223DFAE234879D601D3;MiAppStore;_f1qJucbm08DiWYZ;) MIOTStore/20191212 ("
-                     "YouPin;4.15.0;0D2F912ED0CA5ECA432C98814A0F1842;20210623194554;A"
-                     ";49CB1FE12BE10DDE7B6D5223DFAE234879D601D3;MiAppStore;_f1qJucbm08DiWYZ;) ",
-        "Cookie":"userId=+86"+phone+"; deviceId="+dev_id+"; sdkVersion=accountsdk-2020.01.09"
+        "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 8.1.0; MI 8 MIUI/8.6.18) APP/xiaomi.youpin APPV/300 MK/TUkgOA== "
+                      "PassportSDK/3.3.5 passport-ui/3.3.5 Dalvik/2.1.0 (Linux; U; Android 8.1.0; MI 8 "
+                      "MIUI/8.6.18)Mobile MIOTWeex/2.0.2 ("
+                      "YouPin;4.15.0;0D2F912ED0CA5ECA432C98814A0F1842;0.20.1;A"
+                      ";49CB1FE12BE10DDE7B6D5223DFAE234879D601D3;MiAppStore;_f1qJucbm08DiWYZ;) MIOTStore/20191212 ("
+                      "YouPin;4.15.0;0D2F912ED0CA5ECA432C98814A0F1842;20210623194554;A"
+                      ";49CB1FE12BE10DDE7B6D5223DFAE234879D601D3;MiAppStore;_f1qJucbm08DiWYZ;) ",
+        "Cookie": "userId=+86" + phone + "; deviceId=" + dev_id + "; sdkVersion=accountsdk-2020.01.09"
     }
     resp = requests.get(url, headers=headers)
     r = resp.text[11:]
@@ -47,13 +48,13 @@ def service_login_auth(device_id, phone, pwd):
     hashpwd = hashmd5(pwd)
     bodys = {
         "cc": "+86",
-        "qs":sveloginresp["qs"],
-        "callback":sveloginresp["callback"],
+        "qs": sveloginresp["qs"],
+        "callback": sveloginresp["callback"],
         "_json": "true",
         "_sign": sveloginresp["_sign"],
-        "user" : "+86"+phone,
-        "hash" : hashpwd,
-        "sid"  : sveloginresp["sid"]
+        "user": "+86" + phone,
+        "hash": hashpwd,
+        "sid": sveloginresp["sid"]
     }
     enbody = urlencode(bodys)
     resp = requests.post(url, headers=headers, data=enbody, proxies=proxies, verify=False)
@@ -61,20 +62,24 @@ def service_login_auth(device_id, phone, pwd):
     res = json.loads(r)
     return res
 
-#登录获取serviceToken
+
+# 登录获取serviceToken
 def callback_auth(dev_id, phone, pwd):
-    cbauth_resp = service_login_auth(dev_id, phone, pwd)
-    ssecurity = cbauth_resp["ssecurity"]
-    nonce = int(float(cbauth_resp["nonce"]))
-    query = "nonce=" + str(nonce) + "&" + ssecurity
-    clientSign = param_gen.hashsha1(query)
-    t = {
-        "clientSign": clientSign
-    }
-    e = urlencode(t)
-    url = cbauth_resp["location"] + "&_userIdNeedEncrypt=true&" + e
-    resp = requests.get(url)
-    r = resp.text[11:]
-    srvtoken = resp.cookies.get("serviceToken")
-    cUserId = resp.cookies.get("cUserId")
-    return srvtoken, cUserId
+    try:
+        cbauth_resp = service_login_auth(dev_id, phone, pwd)
+        ssecurity = cbauth_resp["ssecurity"]
+        nonce = int(float(cbauth_resp["nonce"]))
+        query = "nonce=" + str(nonce) + "&" + ssecurity
+        clientSign = param_gen.hashsha1(query)
+        t = {
+            "clientSign": clientSign
+        }
+        e = urlencode(t)
+        url = cbauth_resp["location"] + "&_userIdNeedEncrypt=true&" + e
+        resp = requests.get(url)
+        r = resp.text[11:]
+        srvtoken = resp.cookies.get("serviceToken")
+        cUserId = resp.cookies.get("cUserId")
+        return srvtoken, cUserId
+    except:
+        print(phone, "登录失败")
--- xiaomiyoupin/crack_program/main.py
+++ xiaomiyoupin/crack_program/main.py
@@ -1,5 +1,8 @@
+import _thread
 import re
 import time
+
+import numpy as np
 
 import param_gen
 import login
@@ -17,37 +20,83 @@ phone = "19160384323"
 pwd = "miss1183989659"
 youpindistinct = "1795beacd71-0c2ad86ced3b82-90d1"
 youpinsession = "179790965b0-0b0a4d1d5ee787-6677"
-#login.callback_auth(dev_id, phone, pwd)
-#spike.doOrder(dev_id, phone, pwd, youpindistinct, youpinsession)
+# login.callback_auth(dev_id, phone, pwd)
+# spike.doOrder(dev_id, phone, pwd, youpindistinct, youpinsession)
 
 serviceToken = ""
 cUserId = ""
 
 
-def job():
-    global serviceToken
-    global cUserId
-    if serviceToken == "":
-        serviceToken, cUserId = login.callback_auth(dev_id, phone, pwd)
-        count = 0
-    #tstr = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-    tstr = '2021-07-02 09:59:59.781286'
-    t = float(time.mktime(time.strptime(tstr, '%Y-%m-%d %H:%M:%S.%f')))
-    while True:
-        now = time.time()
-        if now > t:
-            print(u"到点")
-            break
-    while True:
-        spike.doSpike(youpindistinct, youpinsession, serviceToken, cUserId)
-        count += 1
-        time.sleep(0.1)
-        if count > 20:
-            break
+class Account:
+    def __init__(self, phone, pwd, dev_id):
+        self.phone = phone
+        self.pwd = pwd
+        self.dev_id = dev_id
 
+
+
+    def job(self):
+        global serviceToken
+        global cUserId
+        if serviceToken == "":
+            serviceToken, cUserId = login.callback_auth(self.dev_id, self.phone, self.pwd)
+            count = 0
+        # tstr = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+        tstr = '2021-08-23 09:59:59.781286'
+        t = float(time.mktime(time.strptime(tstr, '%Y-%m-%d %H:%M:%S.%f')))
+        while True:
+            now = time.time()
+            if now > t:
+                print(u"到点")
+                break
+        while True:
+            spike.doSpike(youpindistinct, youpinsession, serviceToken, cUserId)
+            count += 1
+            time.sleep(0.1)
+            if count > 20:
+                break
+
+
+def job(phone, pwd, dev_id):
+        global serviceToken
+        global cUserId
+        if serviceToken == "":
+            serviceToken, cUserId = login.callback_auth(dev_id, phone, pwd)
+            count = 0
+        # tstr = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+        tstr = '2021-08-24 09:59:59.781286'
+        t = float(time.mktime(time.strptime(tstr, '%Y-%m-%d %H:%M:%S.%f')))
+        while True:
+            now = time.time()
+            if now > t:
+                print(u"到点")
+                break
+        while True:
+            spike.doSpike(youpindistinct, youpinsession, serviceToken, cUserId)
+            count += 1
+            time.sleep(0.1)
+            if count > 20:
+                break
 # scheduler = BlockingScheduler()
 # scheduler.add_job(job, 'cron', hour=9, minute=59, second=10)
 # print("start timer")
 # scheduler.start()
 
-job()
+try:
+    _thread.start_new_thread(job, ("18875146084", "Ly080330..", "_f1qJucbm08DiWZA"))
+    _thread.start_new_thread(job,("19160384323", "miss1183989659", "_f1qJucbm08DiWZA"))
+except:
+    print("创建线程失败")
+
+while 1:
+    pass
+#x = Account("19160384323", "miss1183989659", "_f1qJucbm08DiWZA")
+
+
+#
+# fp = open("acc.txt", "rt", encoding="UTF-8")
+# lines = fp.readlines()
+# lineCount = len(lines)
+# dataArr = np.zeros(lineCount)
+
+#spike.doOrder(dev_id, phone, pwd, youpindistinct, youpinsession)
